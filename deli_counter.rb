093fd079573032da87d6339fc302e1e2124e2a82
Antony Sanders <antony.devon92@gmail.com>
2019-11-14 00:30:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def line(customer)
+  counter = 1
+  array = []
+  if customer.empty?
+    puts "The line is currently empty."
+  else
+    customer.each do |place|
+      array.push("#{counter}. #{place}")
+      counter += 1
+   end 
+    puts "The line is currently: #{array.join(" ")}"
+  end
+end
+
+def take_a_namber(deli, customer)
+  if deli.empty?
+    puts "Welcome, #{customer}. You are number 1 in line."
+  end
+end
